8dc377d4e7f38c303c4e28933e55337b624232ac
Luis Hurtado <luisfelipehurtado@gmail.com>
2015-10-21 12:04:53 -0500

    Setup project config and dependencies
    Setup dependencies on HTTPoison and exvcr for tests.
    Elixir version set to ~>1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,9 @@ defmodule Harvest.Mixfile do
   def project do
     [app: :harvest,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.1",
+     description: "Harvest Time Tracking API wrapper written in Elixir",
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -14,7 +16,7 @@ defmodule Harvest.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +29,17 @@ defmodule Harvest.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:httpoison, "~> 0.7.4"},
+     {:poison,    "~> 1.5.0"},
+     {:exvcr,     "~> 0.5.1", only: :test},
+     {:earmark,   "~> 0.1",   only: :docs},
+     {:ex_doc,    "~> 0.9",   only: :docs},
+     {:inch_ex,   "~> 0.4",   only: :docs}]
+  end
+
+  defp package do
+    [contributors: ["luishurtado"],
+     licenses:     ["MIT"],
+     links:        %{"GitHub" => "https://github.com/luishurtado/harvest"} ]
   end
 end
